07db1f6c2c2242e5af8fb7060764ff49b91567bb
Grace lungu <kivubox@gmail.com>
2019-01-24 17:00:17 +0300

    Voting page added

--- UI/Parties/parties.js
+++ UI/Parties/parties.js
@@ -16,40 +16,66 @@ class Parties{
 
     toggleCreation(){
         if(this.createPartie.style.display == "block"){
-            this.createPartie.style.display = "none"; 
+            this.createPartie.style.display = "none";
+            Element("create-btn-icon").style.display = "block";
+            Element("create-btn-text").innerHTML = "CREATE A PARTY";
+            this.reset();   
         }else{
             this.createPartie.style.display = "block";
+            Element("create-btn-icon").style.display = "none";
+            Element("create-btn-text").innerHTML = `<img width="auto" height="12px" src="../assets/imgs/icons/up.png" alt=""/> `;
         }
+    }
+
+    reset(){
+        Element("name").value = "";
+        Element("adress").value = "";
+        Element("save-btn").innerHTML = "SAVE";
+    }
+
+    edit(){
+
+        let partie = {
+            name:"AFDC",
+            hqadress:"Ullamcorper. Street Roseville"
+        };
+
+        Element("name").value = partie.name;
+        Element("adress").value = partie.hqadress;
+        Element("save-btn").innerHTML = "EDIT";
+
+        if(this.createPartie.style.display == "none"){
+            this.createPartie.style.display = "block";
+        }
+
     }
 
     saveSettings(){
 
-        let user = {
+        let partie = {
             name:Element("name").value,
             hqadress:Element("adress").value
         }
     
         let valid = true;
     
-        if(!RegExp(/^[a-zA-Z'-]{2,30}$/).test(user.firstname.toString().trim())){
-            Element('first-name-error').innerHTML = "Invalid first name";
+        if(!RegExp(/^[a-zA-Z0-9'-/,\s]{2,30}$/).test(partie.name.toString().trim())){
+            Element('name-error').innerHTML = "Invalid name";
             valid = false ;
         }else{
-            Element('first-name-error').innerHTML = "";
+            Element('name-error').innerHTML = "";
         }
     
-        if(!RegExp(/^[a-zA-Z'-]{2,30}$/).test(user.lastname.toString().trim())){
-            Element('last-name-error').innerHTML = "Invalid last name";
+        if(!RegExp(/^[a-zA-Z0-9'-/,\s]{2,50}$/).test(partie.hqadress.toString().trim())){
+            Element('adress-error').innerHTML = "Invalid adress";
             valid = false ;
         }else{
-            Element('last-name-error').innerHTML = "";
+            Element('adress-error').innerHTML = "";
         }
     
         if(valid){
     
-            this.toast.show("Settings saved", false);
-    
-            console.log(user);
+            console.log(partie);
     
         }
     
