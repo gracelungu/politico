99c67c543713e67e7ddfea0d894b50ed59af77c6
Grace lungu <kivubox@gmail.com>
2019-01-28 18:05:22 +0300

    .travis.yml updated

--- V1/app.js
+++ V1/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const app = express();
 
-app.get('/', (req,res)=>{
-   res.status(200).send("hello");
+app.get('/', (req, res) => {
+  res.status(200).send('hello');
 });
 
 const port = process.env.PORT || 3000;
-var server = app.listen(port, () => console.log(`Listening on port ${port}...`));
+const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
