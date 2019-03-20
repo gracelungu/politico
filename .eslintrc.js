702330ad53b2e17298bddcbbdda7d876b8c4d0cc
Grace Lungu <gracelungu@Graces-MacBook-Pro.local>
2019-03-20 14:59:28 +0200

    Add cross origins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,17 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-        "jasmine": true
-    }
+  env: {
+    browser: true,
+    es6: true,
+    jasmine: true
+  },
+  extends: "airbnb-base",
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  rules: {}
 };
--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 // External modules
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 // Internal modules
-import party from './server/routes/party';
-import office from './server/routes/office';
-import user from './server/routes/user';
+import party from "./server/routes/party";
+import office from "./server/routes/office";
+import user from "./server/routes/user";
 
 // Express setup
 const app = express();
@@ -14,24 +15,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Express routing
-app.use('/', party);
-app.use('/', office);
-app.use('/', user);
+// Allow origins with cors
+app.use(cors());
 
-//Rendering the Documentation
-app.use('/api/v1/doc',(req, res)=>{
-    res.sendFile(`${__dirname}/doc.html`);
+// Express routing
+app.use("/", party);
+app.use("/", office);
+app.use("/", user);
+
+// Rendering the Documentation
+app.use("/api/v1/doc", (req, res) => {
+  res.sendFile(`${__dirname}/doc.html`);
 });
 
-app.use('*',(req, res)=>{
-    res.status(400).json({
-        status:400,
-        error: 'Wrong http request'
-    });
+app.use("*", (req, res) => {
+  res.status(400).json({
+    status: 400,
+    error: "Wrong http request"
+  });
 });
 
 const port = process.env.PORT || 3003;
 const server = app.listen(port);
 
-export default server ;
+export default server;
